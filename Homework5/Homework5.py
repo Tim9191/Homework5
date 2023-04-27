3c54e7908845236a2b496dbd6edc9b7d586350ec
Teymur <teymur_aliyev@hotmail.com>
2023-04-27 18:36:03 +0400

    Add project files.

---
+++ Homework5/Homework5.py
@@ -0,0 +1,29 @@
+#Task 26
+
+#def power(a, b):
+#    if b == 0:
+#        return 1
+#    elif b % 2 == 0:
+#        return power(a, b//2) * power(a, b//2)
+#    else:
+#        return a * power(a, b//2) * power(a, b//2)
+
+#a = int(input("Enter a: "))
+#b = int(input("Enter b: "))
+
+#print(f"{a} to the power of {b} is {power(a, b)}")
+
+
+#Task 28
+
+#def sum(a, b):
+#    if b == 0:
+#        return a
+#    elif a == 0:
+#        return b
+#    else:
+#        return sum(a+1, b-1)
+
+
+
+
